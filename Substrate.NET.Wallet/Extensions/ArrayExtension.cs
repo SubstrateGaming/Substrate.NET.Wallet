425ca1a99f6d3c05b55ffdbefb41f6ab11c64c19
Apolixit <friotromain@gmail.com>
2023-10-11 07:14:32 +0200

    Move code from Substrate.Net.API. Bump dependencies (Net.API to 0.9.9) Test project to .Net 6 Use CryptSharpStandard to Scrypt algorithm

---
+++ Substrate.NET.Wallet/Extensions/ArrayExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Substrate.NET.Wallet.Extensions
+{
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Read an array from start index to end index
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static T[] SubArray<T>(this T[] array, int start, int end)
+        {
+            int length = end - start;
+            if (length < 0)
+                throw new InvalidOperationException($"{nameof(SubArray)} has start invalid start / end");
+
+            T[] result = new T[length];
+            Array.Copy(array, start, result, 0, length);
+            return result;
+        }
+
+        public static T[] SubArray<T>(this T[] array, int start) => array.SubArray(start, array.Length);
+    }
+}
---
+++ Substrate.NET.Wallet/Extensions/BytesExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Substrate.NET.Wallet.Extensions
+{
+    public static class BytesExtension
+    {
+        /// <summary>
+        /// Load a byte array with random bytes
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] Populate(this byte[] value)
+        {
+            new Random().NextBytes(value);
+            return value;
+        }
+
+        public static byte[] BytesFixLength(this byte[] value, int bitLength = -1, bool atStart = false)
+        {
+            int byteLength = (int)Math.Ceiling((double)bitLength / 8);
+
+            if (bitLength == -1 || value.Length == byteLength)
+                return value;
+            else if (value.Length > byteLength)
+                return value.Take(byteLength).ToArray();
+
+            byte[] result = new byte[byteLength];
+
+            if (atStart)
+                Array.Copy(value, 0, result, 0, value.Length);
+            else
+                Array.Copy(value, 0, result, byteLength - value.Length, value.Length);
+
+            return result;
+        }
+    }
+}
